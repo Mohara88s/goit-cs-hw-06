fe934203afe4268ddb9f02c6507b62c5df2f6ba1
Vitalii Vasylets <mohara88s@gmail.com>
2025-07-22 11:28:56 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # goit-cs-hw-06
+
+python -m venv venv
+
+.\venv\Scripts\activate.ps1
+
+. python -m pip freeze > requirements.txt
---
+++ main.py
@@ -0,0 +1,65 @@
+import mimetypes
+import pathlib
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+
+HTTPServer_IP = '127.0.0.1'
+HTTPServer_PORT = 3000
+UDP_IP='127.0.0.1'
+UDP_PORT = 5000
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('./front/index.html')
+        elif pr_url.path == '/message':
+            self.send_html_file('./front/message.html')
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file('./front/error.html', 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = (HTTPServer_IP, HTTPServer_PORT)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+        print(f'HTTPServer started at http://{HTTPServer_IP}:{HTTPServer_PORT}/')
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == '__main__':
+    run()
